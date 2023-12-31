29cd5200dc7a1e5b69c13902c76b5cbc952cf9b4
Hasnaat Iftikhar <hasnaatfreelancing@gmail.com>
2024-01-01 02:08:40 +0500

    :bug: Fix - DB Connection fixing

--- src/db/index.js
+++ src/db/index.js
@@ -3,7 +3,7 @@ import mongoose from "mongoose";
 const connectDB = async () => {
   try {
     const connectionInstance = await mongoose.connect(
-      `${process.env.MONGODB_URI}`
+      `${process.env.MONGODB_URI}/${DB_NAME}`
     );
     console.log(
       "-----------------------\nConnection successful!!\n-----------------------"
